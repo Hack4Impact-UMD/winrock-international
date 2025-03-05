f0d8b19053940d9b09008fc5618478fe87a1c813
asea-aranion <82777688+asea-aranion@users.noreply.github.com>
2025-03-04 22:19:55 -0500

    Implement and test basic proposal form querying function

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,19 +1,43 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import { db } from "../../../frontend/src/firebaseConfig.js";
+import { getDoc, doc } from "firebase/firestore";
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const getAgForm = onRequest(async (request, response) => {
+
+	try {
+
+		// read document ID parameter
+		// const docID = request.query.documentId as string;
+		// if (!docID) {
+		// 	response.status(400).send({
+		// 		error: "Missing documentId parameter"
+		// 	});
+		// }
+		const docID = "tTMuoxVae4Ut15QlAZIN";
+
+		// get a test question response
+		
+		const mainIntervention = await getDoc(doc(db, 
+			`agriculture-form/${docID}/questions/activity-description/category-questions/main-intervention`));
+
+		const mainInterventionResponse = mainIntervention.data();
+		console.log(mainInterventionResponse);
+
+		if (mainInterventionResponse) {
+			logger.log(mainInterventionResponse);
+		}
+
+		response.status(200).send(JSON.stringify(mainInterventionResponse));
+	}
+	catch(error) {
+
+		// log any errors encountered during read
+		logger.error("Error fetching agriculture form details:", error);
+		response.status(500).send({
+			error: "Internal server error"
+		});
+	}
+});
