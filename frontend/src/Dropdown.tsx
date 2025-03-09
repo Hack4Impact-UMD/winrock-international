849f5c25c9ecf26a55280cd8f60eb46297978631
Suhani <suhanigarima@gmail.com>
2025-03-08 20:23:25 -0500

    dropdown component updated

--- frontend/src/Dropdown.tsx
+++ frontend/src/Dropdown.tsx
@@ -1,15 +1,53 @@
 
+import { useState } from "react";
 import styles from "./Dropdown.module.css"
 
-export interface DropdownProps {
-	question: string
+interface DropdownProps {
+	question: string;
+	options: string[];
 }
 
-export const Dropdown = (props: DropdownProps) => {
+const Dropdown = (props: DropdownProps) => {
+	const [showDropdown, setShowDropdown] = useState<boolean>(false);
+	const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+	const toggleDropDown = () => {
+		console.log("Hello");
+		setShowDropdown(!showDropdown);
+	};
+
+	const selectOption = (option: string) => {
+		console.log("Selected Option:", option);
+		setSelectedOption(option);
+		setShowDropdown(false);
+	};
 
 	return (
-		<div>
+		<div className={styles.dropdownContainer}>
 			<h3 className={styles.question}>{props.question}</h3>
+			<button
+				className={styles.dropdownButton}
+				onClick={toggleDropDown}
+			>
+				{selectedOption || "Select"}
+			</button>
+
+			{showDropdown && (
+				<div className={styles.dropdownMenu}>
+					{props.options.map((option) => (
+						<div
+							key={option}
+							className={styles.dropdownItem}
+							onClick={() => selectOption(option)}
+						>
+							{option}
+						</div>
+					))}
+				</div>
+			)}
+
 		</div>
 	);
-}
+};
+
+export default Dropdown;
