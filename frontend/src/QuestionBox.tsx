03bfb374edb5ff63cbbba36fe182a564fbac5a18
sgubba1 <sgubba15@gmail.com>
2025-03-24 14:17:01 -0700

    changed the component to be more focused on width I have a prompt in rn and that can be changed once questions are available

--- frontend/src/QuestionBox.tsx
+++ frontend/src/QuestionBox.tsx
@@ -7,19 +7,18 @@ interface QuestionBoxProps {
 
 const QuestionBox: React.FC<QuestionBoxProps> = ({ prompt }) => {
   return (
-    <div className="question-box">
-      <h2 className="question-title">Climate Change Adaptation</h2>
-      <p className="prompt">{prompt}</p>
-      <select className="dropdown">
-        <option value="">Select</option>
-        <option value="yes">Yes</option>
-        <option value="no">No</option>
-        <option value="partial">Partially</option>
-      </select>
-      <p className="justify">
-        Please justify your answer, including if a mitigation plan is in place.
-      </p>
-      <textarea placeholder="Type here" className="textbox" />
+    <div className="question-box-wrapper">
+      <div className="question-box">
+        <h2 className="question-title">Climate Change Adaptation</h2>
+        <p className="prompt">{prompt}</p>
+        <select className="dropdown">
+          <option value="">Select</option>
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+          <option value="partial">Partially</option>
+        </select>
+      
+      </div>
     </div>
   );
 };
