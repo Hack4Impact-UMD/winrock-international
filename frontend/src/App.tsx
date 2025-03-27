5a3e745e69bf4e550e7b046a3986907bf96c3c9f
Suhani <suhanigarima@gmail.com>
2025-03-27 18:31:23 -0400

    config file with dummy firebase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import Dropdown, { DropdownProps } from "./components/Dropdown";
-import AddDropdown from "./components/AddDropdown";
 
 function App() {
-    const [dropdowns, setDropdowns] = useState<DropdownProps[]>([]);
 
-    return (
-        <>
-            <AddDropdown
-                onAdd={(props: DropdownProps) =>
-                    setDropdowns([...dropdowns, props])
-                }
-            />
+return (
+  <>
 
-            {dropdowns.map((question) => (
-                <>
-                    <Dropdown
-                        id={question.id}
-                        question={question.question}
-                        options={question.options}
-                    />
-                </>
-            ))}
-        </>
-    );
+  </>
+);
 }
 
 export default App;
