97729d57b60f604778655709c0f7b0862aa5da01
om-arya <om.arya0577@gmail.com>
2025-03-28 12:48:53 -0400

    Create TitleHeader component w/ styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TitleHeader from "./components/TitleHeader";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <TitleHeader
+      title="Risk and Co-Benefit Form"
+      description="Some spiel about what this form is about and just to provide some further information about what’s happening?"
+    />
   )
 }
 
-export default App
+export default App;
