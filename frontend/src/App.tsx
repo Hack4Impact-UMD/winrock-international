26b2f595e4f68b5fb5022120c6b659920eff2095
om-arya <om.arya0577@gmail.com>
2025-03-28 13:09:26 -0400

    Create LogoHeader component w/ styling and edit TitleHeader styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import LogoHeader from "./components/LogoHeader";
 import TitleHeader from "./components/TitleHeader";
 
 function App() {
   return (
-    <TitleHeader
-      title="Risk and Co-Benefit Form"
-      description="Some spiel about what this form is about and just to provide some further information about what’s happening?"
-    />
+    <>
+      <LogoHeader />
+      <TitleHeader
+        title="Risk and Co-Benefit Form"
+        description="Some spiel about what this form is about and just to provide some further information about what’s happening?"
+      />
+    </>
   )
 }
 
