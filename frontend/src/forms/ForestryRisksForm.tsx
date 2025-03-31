68e7a50cd1d83a868c44b032be716ecd221be682
Rishi Agarwal <rishiagarwal1313@gmail.com>
2025-03-31 17:31:03 -0400

    Finished Page 3 of Risk Form and Updated CoBenefit Form

--- frontend/src/forms/ForestryRisksForm.tsx
+++ frontend/src/forms/ForestryRisksForm.tsx
@@ -12,7 +12,7 @@ import '../css-modules/ForestryRisksForm.css'
 function ForestryRisksForm() {
    const [currentPage, setCurrentPage] = useState(1)
    const [isSubmitted, setIsSubmitted] = useState(false)
-   const totalPages = 3
+   const totalPages = 4
 
    const nextPage = () => {
       if (currentPage < totalPages) {
@@ -61,15 +61,17 @@ function ForestryRisksForm() {
          <ProgressBar
             currentPage={currentPage}
             totalPages={totalPages}
-            pageLabels={['Risk Form (Page 1)', 'Risk Form (Page 2)', 'Co-benefit form']}
+            pageLabels={['Risk Form (Page 1)', 'Risk Form (Page 2)', 'Risk Form (Page 3)', 'Co-benefit Form']}
          />
 
          {/* Render different pages based on currentPage state */}
          {currentPage === 1 && <PageOne handleSelect={handleSelect} />}
 
          {currentPage === 2 && <PageTwo handleSelect={handleSelect} />}
+         
+         {currentPage === 3 && <PageThree handleSelect={handleSelect} />}
 
-         {currentPage === 3 && <CoBenefitForm handleSelect={handleSelect} />}
+         {currentPage === 4 && <CoBenefitForm handleSelect={handleSelect} />}
 
          <NavigationButtons
             onNext={nextPage}
@@ -311,43 +313,215 @@ const PageTwo = ({ handleSelect }) => {
    )
 }
 
+const PageThree = ({ handleSelect }) => {
+   return (
+      <>
+         <SectionHeader label = 'Human and Labor Rights' />
+         <Dropdown 
+            id = {"protection_human_rights"}
+            question={
+               'Does this project align with Nestle\'s "Responsible Sourcing Core Requirements" framework for the protection of Human Rights? '
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('protection_human_rights', value)}
+            documentLink={'https://www.nestle.com/sites/default/files/asset-library/documents/library/documents/suppliers/nestle-responsible-sourcing-standard-english.pdf'}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'protection_human_rights_details'}
+            response={''}
+         />
+
+         <Dropdown 
+            id = {"formal_declarations"}
+            question={
+               'Are there formal declarations, accountability frameworks and/or third party inspections that ensure that no forced or child labor is involved in the proposed interventions?'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('formal_declarations', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'formal_declarations_deatils'}
+            response={''}
+         /> 
+
+         <Dropdown 
+            id = {"stakeholder_health_safety"}
+            question={
+               'Are there assurances for stakeholder health and safety in place?'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('stakeholder_health_safety', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'stakeholder_health_safety_details'}
+            response={''}
+         />
+
+         <SectionHeader label = 'Community Impacts'/>
+         <Dropdown 
+            id = {"indigenous_people"}
+            question={
+               'Has this project been designed to respect and comply with applicable laws and human rights (statutory and/or customary)  regarding the acquisition, leasing and/or land use change of lands and natural resources of Indigenous peoples and local communities that are impacted or potentially impacted?'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('indigenous_people', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'indigenous_people_details'}
+            response={''}
+         />
+         
+         <Dropdown 
+            id = {"reduce_stakeholder_risk"}
+            question={
+               'Has the project been designed to reduce the risk to stakeholder livelihoods (fair payments, incentives)?'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('reduce_stakeholder_risk', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'reduce_stakeholder_risk_details'}
+            response={''}
+         />
+
+         <Dropdown 
+            id = {"small_holders"}
+            question={
+               'Has this project been designed to involve and include small holders?'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('small_holders', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'small_holders_details'}
+            response={''}
+         />
+
+         <Dropdown 
+            id = {"designed_to_invest"}
+            question={
+               'Has this project been designed to invest in the capacity building of the involved stakeholders through training?'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('designed_to_invest', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'designed_to_invest_details'}
+            response={''}
+         />
+
+         <Dropdown 
+            id = {"efforts_to_maintain_engagements"}
+            question={
+               'Is there effort to maintain ongoing engagements and participation of the community and involved farmers with regards to this project, including mechanisms to consider grievances?'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('efforts_to_maintain_engagements', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'efforts_to_maintain_engagements_details'}
+            response={''}
+         />
+
+         <Dropdown 
+            id = {"negative_community_impacts"}
+            question={
+               'Has this project been designed to minimize other potentially negative community impacts?​'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('negative_community_impacts', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'negative_community_impacts_details'}
+            response={''}
+         />
+
+         <SectionHeader label = 'Safeguards' />
+         <Dropdown 
+            id = {"effective_safeguards"}
+            question={
+               'Have effective safeguards been incorporated in the to design phase? ​'
+            }
+            options={['Yes', 'No', 'Not Applicable']}
+            onSelect={value => handleSelect('effective_safeguards', value)}
+         />
+         <TextQuestion
+            name={
+               'Please provide details including if a mitigation plan is in place. If not applicable, please justify.'
+            }
+            id={'effective_safeguards_details'}
+            response={''}
+         />
+
+
+      </>
+   )
+}
+
 const CoBenefitForm = ({ handleSelect }) => {
    return (
       <>
          <SectionHeader label='Project Water Co-Benefits​' />
          <Dropdown
-            id={'safeguards'}
-            question={'Is it expected that the project activities will improve?​'}
+            id={'water-co-benefits'}
+            question={'Is it expected that the project activities will improve resilience to potential water scarcity, water quality (urface or ground water), or water availability in the local community?'}
             options={['Yes', 'No', 'Not Applicable']}
-            onSelect={value => handleSelect('safeguards', value)}
+            onSelect={value => handleSelect('water-co-benefits', value)}
          />
          <TextQuestion
             name={'(If yes, please describe how and how impactful)'}
-            id={'safeguards_details'}
+            id={'water-co-benefits_details'}
             response={''}
          />
-         <SectionHeader label='Project Water Co-Benefits​' />
+         <SectionHeader label='Project Biodiversity and Environmental Co-Benefits​' />
          <Dropdown
-            id={'safeguards'}
-            question={'Is it expected that the project activities will improve?​'}
+            id={'biodiversity_environmental_co_benefits'}
+            question={'Is it expected that the project activities will improve overall species richness and diversity, threatened species, threatened or rare ecosystems, air quality, or soil erosion?​'}
             options={['Yes', 'No', 'Not Applicable']}
-            onSelect={value => handleSelect('safeguards', value)}
+            onSelect={value => handleSelect('biodiversity_environmental_co_benefits', value)}
          />
          <TextQuestion
             name={'(If yes, please describe how and how impactful)'}
-            id={'safeguards_details'}
+            id={'biodiversity_environmental_co_benefits_details'}
             response={''}
          />
-         <SectionHeader label='Project Water Co-Benefits​' />
+         <SectionHeader label='Project Community/Farmer Co-Benefits​' />
          <Dropdown
-            id={'safeguards'}
-            question={'Is it expected that the project activities will improve?​'}
+            id={'community_farmer_co_benefits'}
+            question={'Is it expected that the project activities will improve farmer livelihoods or income generated, farmer resilience to climate change, farmer adaptation to climate change, local livelihoods or increased income generated, community resilience to climate change, orcommunity adaptation to climate change?​'}
             options={['Yes', 'No', 'Not Applicable']}
-            onSelect={value => handleSelect('safeguards', value)}
+            onSelect={value => handleSelect('community_farmer_co_benefits', value)}
          />
          <TextQuestion
             name={'(If yes, please describe how and how impactful)'}
-            id={'safeguards_details'}
+            id={'community_farmer_co_benefits_details'}
             response={''}
          />
       </>
