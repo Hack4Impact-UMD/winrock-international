20db00d99a40449a5ca9fb2c7be53ac42e46b1b8
om-arya <om.arya0577@gmail.com>
2025-03-31 08:28:06 -0400

    Remove the changes in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,35 @@
-import LogoHeader from "./components/LogoHeader";
-import SectionHeader from "./components/SectionHeader";
-import TitleHeader from "./components/TitleHeader";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return (
-    // Example usage of header components
-    <>
-      <LogoHeader />
-      <TitleHeader
-        title="Risk and Co-Benefit Form"
-        description="Some spiel about what this form is about and just to provide some further information about what’s happening?"
-      />
+  const [count, setCount] = useState(0)
 
-      <SectionHeader label="Risk Assessment" />
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
 
-export default App;
+export default App
