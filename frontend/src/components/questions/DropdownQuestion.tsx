42e6c21287a9b6fc3cd1a1ed92e97df14cb50355
Kyle Cho <69482279+kylejcho@users.noreply.github.com>
2025-04-01 18:24:16 -0400

    Dropdown component styling + "shifting" fixed

--- frontend/src/components/questions/DropdownQuestion.tsx
+++ frontend/src/components/questions/DropdownQuestion.tsx
@@ -1,79 +1,88 @@
-import { useState } from "react";;
-import styles from "../../css-modules/DropdownQuestion.module.css";
-import chevron from "../../assets/chevron-up-svgrepo-com.svg";
+import { useState } from 'react'
+import styles from '../../css-modules/DropdownQuestion.module.css'
+import chevron from '../../assets/chevron-up-svgrepo-com.svg'
 
 interface DropdownQuestionProps {
-	label: string;
-	options: string[];
-	onSelect: (selected: string) => void;
-	required?: boolean;
+   label: string
+   options: string[]
+   onSelect: (selected: string) => void
+   required?: boolean
 }
 
-const DropdownQuestion = ({ label, options, onSelect, required = false }: DropdownQuestionProps) => {
-	const [showDropdown, setShowDropdown] = useState<boolean>(false);
-	const [selectedOption, setSelectedOption] = useState<string | null>(null);
-	const [isValid, setIsValid] = useState(true);
+const DropdownQuestion = ({
+   label,
+   options,
+   onSelect,
+   required = false,
+}: DropdownQuestionProps) => {
+   const [showDropdown, setShowDropdown] = useState<boolean>(false)
+   const [selectedOption, setSelectedOption] = useState<string | null>(null)
+   const [isValid, setIsValid] = useState(true)
 
-	const toggleDropdownQuestion = () => {
-		setShowDropdown(true);
-	};
+   const toggleDropdownQuestion = () => {
+      setShowDropdown(true)
+   }
 
-	const selectOption = (option: string) => {
-		setIsValid(true);
-		
-		setSelectedOption(option);
-		setShowDropdown(false);
-		onSelect(option);
-	};
+   const selectOption = (option: string) => {
+      setIsValid(true)
 
-	// Allow the user to backspace to clear a dropdown
-	// This is important for non-required dropdown questions
-	const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
-        if (event.key === "Backspace") {
-            setSelectedOption(null);
-        }
-    };
+      setSelectedOption(option)
+      setShowDropdown(false)
+      onSelect(option)
+   }
 
-	function validate() {
-		if (required && !selectedOption) {
-			setIsValid(false);
-		}
-	}
+   // Allow the user to backspace to clear a dropdown
+   // This is important for non-required dropdown questions
+   const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+      if (event.key === 'Backspace') {
+         setSelectedOption(null)
+      }
+   }
 
-	return (
-		<div className={styles.dropdownQuestion}>
-			<h3 className={`${styles.label} ${required ? styles.required : ""}`}>
-				{label}
-			</h3>
-			<button
-				className={`${styles.dropdownButton} ${isValid ? styles.validDropdownButton : styles.invalidDropdownButton}`}
-				onMouseDown={toggleDropdownQuestion}
-				onBlur={() => {
-					setShowDropdown(false); // clicking off the dropdown closes it
-					validate();
-				}}
-				onKeyDown={handleKeyDown}
-			>
-				<p>{selectedOption || "Select an option"}</p>
-				<img src={chevron} className={styles.chevron} style={{rotate: showDropdown ? "0deg" : "180deg"}}></img>
-			</button>
+   function validate() {
+      if (required && !selectedOption) {
+         setIsValid(false)
+      }
+   }
 
-			{showDropdown && (
-				<div className={styles.dropdownMenu}>
-					{options.map((option) => (
-						<div
-							key={option}
-							className={styles.dropdownItem}
-							onMouseDown={() => selectOption(option)}
-						>
-							{option}
-						</div>
-					))}
-				</div>
-			)}
+   return (
+      <div className={styles.dropdownQuestion}>
+         <h3 className={`${styles.label} ${required ? styles.required : ''}`}>{label}</h3>
+         <div className={styles.dropdownContainer}>
+            <button
+               className={`${styles.dropdownButton} ${
+                  isValid ? styles.validDropdownButton : styles.invalidDropdownButton
+               }`}
+               onMouseDown={toggleDropdownQuestion}
+               onBlur={() => {
+                  setShowDropdown(false) // clicking off the dropdown closes it
+                  validate()
+               }}
+               onKeyDown={handleKeyDown}
+            >
+               <p>{selectedOption || 'Select an option'}</p>
+               <img
+                  src={chevron}
+                  className={styles.chevron}
+                  style={{ rotate: showDropdown ? '0deg' : '180deg' }}
+               ></img>
+            </button>
+            {showDropdown && (
+               <div className={styles.dropdownMenu}>
+                  {options.map(option => (
+                     <div
+                        key={option}
+                        className={styles.dropdownItem}
+                        onMouseDown={() => selectOption(option)}
+                     >
+                        {option}
+                     </div>
+                  ))}
+               </div>
+            )}{' '}
+         </div>
+      </div>
+   )
+}
 
-		</div>
-	);
-};
-
-export default DropdownQuestion;
+export default DropdownQuestion
