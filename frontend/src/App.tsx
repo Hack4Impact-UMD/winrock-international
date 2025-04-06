c017af32abd4bede553a41164180fe36cf9df8d2
om-arya <om.arya0577@gmail.com>
2025-04-06 10:45:14 -0400

    Configure test DB w/ test buttons in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { handleClientSignup, handleClientLogin } from "./auth/clientAuth";
+
+function generateRandomString(length: number) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+function generateRandomEmail() {
+  return generateRandomString(10) + "@" + generateRandomString(5) + ".com";
+}
+
+function generateRandomPassword() {
+  return generateRandomString(15);
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentUserEmail, setCurrentUserEmail] = useState(""); 
+  const [currentUserPassword, setCurrentUserPassword] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p
+        style={{ margin: "1.5rem", fontSize: "2rem" }}
+      >
+        Test Client Signup/Login
       </p>
+      <button
+        onClick={async () => {
+          const newUserEmail = generateRandomEmail();
+          const newUserPassword = generateRandomPassword();
+          setCurrentUserEmail(newUserEmail);
+          setCurrentUserPassword(newUserPassword);
+          await handleClientSignup({
+            email: newUserEmail,
+            password: newUserPassword
+          });
+        }}
+        style={{ margin: "1rem", padding: "1rem 2rem", borderRadius: "15px", background: "blue", color: "white",
+                 fontSize: "1rem", fontFamily: "sans-serif", cursor: "pointer"}}>
+          Create User w/ Random Email and Password
+      </button>
+
+      <button
+        onClick={async () => {
+          await handleClientLogin({
+            email: currentUserEmail,
+            password: currentUserPassword
+          });
+        }}
+        style={{ padding: "1rem 2rem", borderRadius: "15px", background: "white", color: "black",
+                 fontSize: "1rem", fontFamily: "sans-serif", cursor: "pointer"}}>
+        Log In User (Click After Creating User)
+      </button>
     </>
   )
 }
--- frontend/src/auth/clientAuth.ts
+++ frontend/src/auth/clientAuth.ts
@@ -1,22 +1,17 @@
-import { auth } from "../firebaseConfig.js";
+import { auth } from "../testFirebaseConfig.js";
 import { type User, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 interface ClientSignupInfo {
-    firstName: string;
-    lastName: string;
     email: string;
     password: string;
 }
 
 interface ClientLoginInfo {
-    firstName: string;
-    lastName: string;
     email: string;
     password: string;
 }
 
-// https://firebase.google.com/docs/auth/web/start
-async function handleClientSignup({ firstName, lastName, email, password }: ClientSignupInfo) {
+async function handleClientSignup({ email, password }: ClientSignupInfo) {
     try {
         // Firebase automatically updates the user to be used throughout the app.
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
@@ -24,20 +19,21 @@ async function handleClientSignup({ firstName, lastName, email, password }: Clie
 
         // Firebase manages users via email and password.
         // To track other info e.g. firstName and lastName, we'll need to store it in the Firestore database.
+
+        console.log(user);
     } catch (error) {
         console.error("Error signing up: ", error);
     }
 }
 
-// https://firebase.google.com/docs/auth/web/start
 async function handleClientLogin({ email, password }: ClientLoginInfo) {
     try {
-        // Firebase automatically updates the user to be used throughout the app.
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
         const user: User = userCredential.user;
-
-        // We can use user.uid to access fields like firstName and lastName from Firestore.
+        console.log(user);
     } catch (error) {
         console.error("Error signing in: ", error);
     }
 }
+
+export { handleClientSignup, handleClientLogin }
