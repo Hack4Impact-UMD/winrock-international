e048d6cb72d89bf33f65fa27f79b0993cb71bb55
om-arya <om.arya0577@gmail.com>
2025-04-19 18:18:28 -0400

    Copy current files for merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
-} from 'react-router-dom';
+import WinrockDashboard from './dashboards/winrockDashboard';
 
-import LoginPage   from './auth/pages/LoginPage';
-import ForgotPasswordPage from './auth/pages/ForgotPasswordPage';
-import SignupPage from './auth/pages/SignupPage';
-
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signup"                     element={<SignupPage />} />
-        <Route path="/"                           element={<LoginPage />} />
-        <Route path="/forgot-password"            element={<ForgotPasswordPage />} />
-        <Route path="*"                           element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="app-container">
+      <WinrockDashboard />
+    </div>
   );
-  
 }
+
+export default App;
