6d5a1c8ecf6ca377522c9be7fccfbfb80ba49c43
om-arya <om.arya0577@gmail.com>
2025-04-21 14:25:07 -0400

    Improve get functions + add documentation

--- frontend/src/dashboards/winrock-dashboard/winrockDashboardService.ts
+++ frontend/src/dashboards/winrock-dashboard/winrockDashboardService.ts
@@ -6,7 +6,6 @@ import {
     getDoc,
     getDocs,
     orderBy,
-    Query,
     query,
     setDoc,
     Timestamp,
@@ -16,6 +15,10 @@ import {
 import { db } from "../../firebaseConfig.js";
 import Result from "../../types/Result.js";
 
+
+/**
+ * Represents the overall status of a project.
+ */
 enum OverallStatus {
     ON_TRACK = "On Track",
     AT_RISK = "At Risk",
@@ -24,6 +27,9 @@ enum OverallStatus {
     COMPLETED_EXCEPT_RISK = "Completed (except for risk)"
 }
 
+/**
+ * Represents the stage of analysis the project is currently in.
+ */
 enum AnalysisStage {
     STAGE_1 = "Stage 1: Clarifying Initial Project Information",
     STAGE_2 = "Stage 2: Clarifying Technical Details",
@@ -33,24 +39,42 @@ enum AnalysisStage {
     STAGE_6 = "Stage 6: Complete, and Excluded"
 }
 
+
+/**
+ * Represents a project stored in the database.
+ */
 interface Project {
     projectName: string;
     supplierName: string;
     spendCategory: string;
     geography: string;
+
     overallStatus: OverallStatus;
     analysisStage: AnalysisStage;
-    startDate: Timestamp; // receive and send as a Date
-    lastUpdated: Timestamp; // receive and send as a Date
+
+    startDate: Timestamp; // Received and sent as a Date
+    lastUpdated: Timestamp; // Received and sent as a Date
+
+    isActive: boolean; // True if the project is active, false if it is archived
+    isPinned: boolean; // True if the project is pinned, false if it is unpinned
 }
 
 /**
- * Saves a project with the given fields
- * into the database.
+ * Saves a project with the given fields into the database.
  * 
- * The projectName must be unique.
+ * @param {string} projectName - Must be unique across all projects.
+ * @param {string} supplierName
+ * @param {string} spendCategory
+ * @param {string} geography
+ * @param {OverallStatus} overallStatus - Defaults to ON_TRACK, but can optionally be set.
+ * @param {AnalysisStage} analysisStage - Defaults to STAGE_1, but can optionally be set.
+ * @param {Date} startDate - Defaults to the current date, but can optionally be set.
+ * @param {isActive} isActive - Defaults to true, but can optionally be set.
+ * @param {isPinned} isPinned - Defaults to false, but can optionally be set.
  * 
- * @returns a Promise<Result>.
+ * @returns {Promise<Result>} A promise resolving to a `Result` object:
+ *      - On success: `{ success: true }`
+ *      - On failure: `{ success: false, errorCode: string }`
  */
 const createProject = async (
     projectName: string,
@@ -59,7 +83,9 @@ const createProject = async (
     geography: string,
     overallStatus: OverallStatus = OverallStatus.ON_TRACK,
     analysisStage: AnalysisStage = AnalysisStage.STAGE_1,
-    startDate?: Date
+    startDate?: Date,
+    isActive: boolean = true,
+    isPinned: boolean = false
 ): Promise<Result> => {
     try{
         const docRef = doc(db, "projects", projectName);
@@ -77,7 +103,9 @@ const createProject = async (
             overallStatus,
             analysisStage,
             startDate: startDate ? Timestamp.fromDate(startDate) : now,
-            lastUpdated: now
+            lastUpdated: now,
+            isActive,
+            isPinned
         };
         await setDoc(docRef, newProject);
 
@@ -91,12 +119,13 @@ const createProject = async (
 }
 
 /**
- * Retrieves the project with the given name.
- * If the project does not exist, an error
- * is returned.
+ * Retrieves the project with the given name. If the project does not exist, an error is returned.
  * 
- * @returns a Promise<Result> containing the
- *          requested project.
+ * @param {string} projectName - The name of the project to retrieve.
+ * 
+ * @returns {Promise<Result>} A promise resolving to a `Result` object:
+ *      - On success: `{ success: true, data: project }`
+ *      - On failure: `{ success: false, errorCode: string }`
  */
 const getProjectByName = async (projectName: string): Promise<Result> => {
     try {
@@ -125,123 +154,87 @@ const getProjectByName = async (projectName: string): Promise<Result> => {
     }
 }
 
-/*
-To load data on component mount:
-	const [projects, setProjects] = useState<ProjectData[]>([]);
-	const [names, setNames] = useState<string[]>([]);
+/**
+ * Retrieves all projects (along with a list of their names) from Firestore.
+ * 
+ * @param orderByField - The name of a field by which to order the results.
+ * @param desc - Whether to order the results in descending order.
+ * 
+ * @returns {Promise<Result>} A promise resolving to a `Result` object:
+ *      - On success: `{ success: true, data: { projects: Project[], projectNames: string[] } }`
+ *      - On failure: `{ success: false, errorCode: string }`
+ */
+const getAllProjects = async (orderByField: string, desc: boolean): Promise<Result> => {
+    return getProjectsWithFilters(orderByField, desc, [], []);
+}
 
-	useEffect(() => {
-		const getData = async () => {
-			setProjects(await getProjects("name", true));
-			setNames(await getProjectNamesStartingWith("Ch"));
-		};
-
-		getData();
-	}, []);
-
-To reload data/search:
-	<button onClick={async () => setProjects(await getProjects("name", true))}>Refresh</button>
-*/
-
-// retrieves all projects ordered by [field] asc/desc,
-// only where [filterByField] == [filterValue] (or where geography contains [filterValue]) if present
-
-// to order by another field while filtering by geography with array-contains, a composite index needs to be
-// manually added to the firestore for geography (Arrays) and the order-by field (Ascending/Descending)
-const getProjects = async (orderByField: string, desc: boolean, filterByField?: string, filterValue?: string): Promise<Result> => {
+/**
+ * Retrieves a list of projects (along with a list of their names) from Firestore that match
+ * the specified ordering criteria, filters, and optional date range.
+ *
+ * @param {string} orderByField - The name of a single field by which to order the results.
+ * @param {boolean} desc - Whether to order the results in descending order.
+ * @param {string[]} filterFields - An array of field names to apply filters on.
+ * @param {any[]} filterValues - An array of values corresponding to the filterFields. 
+ *                               Each value can be a single value or an array of allowed values.
+ * @param {Date} [startDate] - Optional start date to filter projects with a 'date' field greater than or equal to this value.
+ * @param {Date} [endDate] - Optional end date to filter projects with a 'date' field less than or equal to this value.
+ * 
+ * @returns {Promise<Result>} A promise resolving to a `Result` object:
+ *      - On success: `{ success: true, data: { projects: Project[], projectNames: string[] } }`
+ *      - On failure: `{ success: false, errorCode: string }`
+ */
+const getProjectsWithFilters = async (
+    orderByField: string,
+    desc: boolean,
+    filterFields: string[],
+    filterValues: any[],
+    startDate?: Date,
+    endDate?: Date
+): Promise<Result> => {
     const projects: Project[] = [];
-    let filterQuery: Query;
-    
+    const projectNames: string[] = []; // Useful for search auto-complete
+
     try {
-        if (filterByField && filterValue) {
-            filterQuery = query(collection(db, "projects"), 
-            where(filterByField, filterByField === "geography" ? "array-contains" : "==", filterValue), 
-            orderBy(orderByField, desc ? "desc" : "asc"));
-        } else {
-            filterQuery = query(collection(db, "projects"),
-            orderBy(orderByField, desc ? "desc" : "asc"));
-        }
+        let filterQuery = query(collection(db, "projects"));
+
+        // Apply filters
+        filterFields.forEach((field, index) => {
+            const value = filterValues[index];
+            if (Array.isArray(value)) {
+                // There are multiple allowed values for this field
+                filterQuery = query(filterQuery, where(field, 'in', value));
+            } else {
+                // There is just one allowed value for this field
+                filterQuery = query(filterQuery, where(field, '==', value));
+            }
+        });
+
+        if (startDate) filterQuery = query(filterQuery, where('date', '>=', Timestamp.fromDate(startDate)));
+        if (endDate) filterQuery = query(filterQuery, where('date', '<=', Timestamp.fromDate(endDate)));
+
+        // Apply ordering
+        filterQuery = query(filterQuery, orderBy(orderByField, desc ? "desc" : "asc"));
 	
 		const querySnapshot = await getDocs(filterQuery);
 		querySnapshot.forEach((doc) => {
-			const convertedDoc = {
+			const project = {
                 ...doc.data(),
                 startDate: doc.data().startDate.toDate(),
                 lastUpdated: doc.data().lastUpdated.toDate()
-            }
-            projects.push(convertedDoc as Project);
+            } as Project;
+            projects.push(project);
+            projectNames.push(project.projectName);
 		});
 
 		return {
             success: true,
-            data: projects
-        };
-	}
-	catch (error) {
-		return {
-            success: false,
-            errorCode: error instanceof FirebaseError ? error.code : "unknown"
-        };
-	}
-}
-
-// retrieves all project names that start with [term] (for use in autocomplete)
-const getProjectNamesStartingWith = async (term: string): Promise<Result> => {
-    const projectNames: string[] = [];
-    
-    try {
-        const nameQuery = query(collection(db, "projects"), 
-            where("projectName", ">=", term), 
-            where("projectName", "<=", term + "\uf8ff")); // End at the last possible name that starts with the term
-
-		const querySnapshot = await getDocs(nameQuery);
-		querySnapshot.forEach((doc) => {
-			projectNames.push(doc.data().projectName);
-		});
-
-		return {
-            success: true,
-            data: projectNames
-        };
-	}
-	catch (error) {
-		return {
-            success: false,
-            errorCode: error instanceof FirebaseError ? error.code : "unknown"
-        };
-	}
-}
-
-// retrieves all projects with start date between [start] and [end] (for use with calendar picker)
-const getProjectsStartedBetween = async (startDate: Date, endDate: Date): Promise<Result> => {
-    const projects: Project[] = [];
-
-    const startTimestamp = Timestamp.fromDate(startDate);
-    const endTimestamp = Timestamp.fromDate(endDate);
-    
-    try {
-        const datesQuery = query(collection(db, "projects"), 
-            where("startDate", ">=", startTimestamp), 
-            where("startDate", "<=", endTimestamp), 
-            orderBy("startDate", "desc"));
-
-        const querySnapshot = await getDocs(datesQuery);
-
-        querySnapshot.forEach((doc) => {
-            const convertedDoc = {
-                ...doc.data(),
-                startDate: doc.data().startDate.toDate(),
-                lastUpdated: doc.data().lastUpdated.toDate()
+            data: {
+                projects: projects,
+                projectNames: projectNames
             }
-            projects.push(convertedDoc as Project);
-        });
-
-        return {
-            success: true,
-            data: projects
         };
-	}
-	catch (error) {
+	} catch (error) {
 		return {
             success: false,
             errorCode: error instanceof FirebaseError ? error.code : "unknown"
@@ -250,13 +243,18 @@ const getProjectsStartedBetween = async (startDate: Date, endDate: Date): Promis
 }
 
 /**
- * Updates the given field of the given project
- * to newData. If the project does not exist,
+ * Updates the given field of the given project to newValue. If the project does not exist,
  * an error is returned.
  * 
- * @returns a Promise<Result>.
+ * @param {string} projectName - The name of the project to update a field of.
+ * @param {string} field - The field to update.
+ * @param {string} newValue - The value to update the field to.
+ * 
+ * @returns {Promise<Result>} A promise resolving to a `Result` object:
+ *      - On success: `{ success: true }`
+ *      - On failure: `{ success: false, errorCode: string }`
  */
-const updateProjectField = async (projectName: string, field: keyof Project, newData: any): Promise<Result> => {
+const updateProjectField = async (projectName: string, field: keyof Project, newValue: any): Promise<Result> => {
     try {
         const docRef = doc(db, `projects/${projectName}`);
         const docSnap = await getDoc(docRef);
@@ -264,13 +262,14 @@ const updateProjectField = async (projectName: string, field: keyof Project, new
             return { success: false, errorCode: "project-not-found"};
         }
 
-        if (newData instanceof Date) {
-            newData = Timestamp.fromDate(newData);
+        // Convert Dates to Firestore Timestamps
+        if (newValue instanceof Date) {
+            newValue = Timestamp.fromDate(newValue);
         }
 
         const updateData = {
-            [field]: newData,
-            lastUpdated: field === "lastUpdated" ? newData : Timestamp.now()
+            [field]: newValue,
+            lastUpdated: field === "lastUpdated" ? newValue : Timestamp.now()
         };
         await updateDoc(docRef, updateData);
 
@@ -284,10 +283,11 @@ const updateProjectField = async (projectName: string, field: keyof Project, new
 };
 
 /**
- * Deletes the given project. If the project
- * does not exist, an error is returned.
+ * Deletes the given project. If the project does not exist, an error is returned.
  * 
- * @returns a Promise<Result>.
+ * @returns {Promise<Result>} A promise resolving to a `Result` object:
+ *      - On success: `{ success: true }`
+ *      - On failure: `{ success: false, errorCode: string }`, with `errorCode` from Firebase or "unknown".
  */
 const deleteProject = async (projectName: string): Promise<Result> => {
     try {
@@ -313,9 +313,8 @@ export {
     AnalysisStage,
     createProject,
     getProjectByName,
-    getProjects,
-    getProjectNamesStartingWith,
-    getProjectsStartedBetween,
+    getAllProjects,
+    getProjectsWithFilters,
     updateProjectField,
     deleteProject
 };
