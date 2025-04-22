d80df31000fe7c68c5714ce37cc1e793f6450c57
asea-aranion <82777688+asea-aranion@users.noreply.github.com>
2025-04-21 22:06:59 -0400

    Create project tracker component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignupPage from './auth/pages/SignupPage';
 import WinrockDashboard from './dashboards/winrock-dashboard/winrockDashboard';
+import ProjectTracker from './dashboards/project-view/components/ProjectTracker';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<SignupPage />} />
-          <Route path="/dashboard" element={<WinrockDashboard />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <div className="app-container">
+    //     <Routes>
+    //       <Route path="/" element={<SignupPage />} />
+    //       <Route path="/dashboard" element={<WinrockDashboard />} />
+    //     </Routes>
+    //   </div>
+    // </BrowserRouter>
+	<ProjectTracker currentStage="Clarifying Initial Project Information" initialInfoStatus='completed' technicalStatus='in-progress' ghgStatus='in-progress' finalStatus='not-started' risksStatus='not-started'></ProjectTracker>
   );
 }
 
