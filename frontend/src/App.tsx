28a2642b49e5627033cbf521484b9ca8064ed338
Debarshi Dey <ddey23@terpmail.umd.edu>
2025-04-21 23:01:21 -0400

    reverted main back to winrock dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignupPage from './auth/pages/SignupPage';
 import WinrockDashboard from './dashboards/winrock-dashboard/winrockDashboard';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<SignupPage />} />
-          <Route path="/dashboard" element={<WinrockDashboard />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="app-container">
+      <WinrockDashboard />
+    </div>
   );
 }
 
