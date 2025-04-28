90d05391c33fc7ca4ca4a2c5ce6a0ae5dbd6faac
Suhani <suhanigarima@gmail.com>
2025-04-27 22:25:02 -0400

    slight changes to confirmation page

--- frontend/src/forms/ConfirmationPage.tsx
+++ frontend/src/forms/ConfirmationPage.tsx
@@ -1,4 +1,4 @@
-import './styles/ConfirmationPage.css'
+import styles from './css-modules/ConfirmationPage.module.css'
 import LogoHeader from './components/headers/LogoHeader'
 
 interface ConfirmationPageProps {
@@ -9,31 +9,31 @@ function ConfirmationPage({ formName }: ConfirmationPageProps) {
    return (
       <>
          <LogoHeader />
-      <div className='form-completed-container'>
-         <div className='completion-content'>
-            <div className='check-icon'>
-               <svg
-                  width='80'
-                  height='80'
-                  viewBox='0 0 80 80'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-               >
-                  <circle cx='40' cy='40' r='40' fill='#31AB48' />
-                  <path
-                     d='M33 45.17L52.59 25.58L56.41 29.41L33 52.83L23.59 43.41L27.41 39.59L33 45.17Z'
-                     fill='white'
-                  />
-               </svg>
+         <div className={styles.formCompletedContainer}>
+            <div className={styles.completionContent}>
+               <div className={styles.checkIcon}>
+                  <svg
+                     width='80'
+                     height='80'
+                     viewBox='0 0 80 80'
+                     fill='none'
+                     xmlns='http://www.w3.org/2000/svg'
+                  >
+                     <circle cx='40' cy='40' r='40' fill='#31AB48' />
+                     <path
+                        d='M33 45.17L52.59 25.58L56.41 29.41L33 52.83L23.59 43.41L27.41 39.59L33 45.17Z'
+                        fill='white'
+                     />
+                  </svg>
+               </div>
+               <h1 className={styles.completionTitle}>
+                  {formName} completed
+               </h1>
+               <p className={styles.completionMessage}>
+                  <i>Your response has been recorded. You may exit the page now.</i>
+               </p>
             </div>
-            <h1 className='completion-title'>
-               {formName} completed
-            </h1>
-            <p className='completion-message'>
-               <i>Your response has been recorded. You may exit the page now.</i>
-            </p>
          </div>
-      </div>
       </>
    )
 }
